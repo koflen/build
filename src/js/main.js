216740cd9a7b59aa2b7ed4cc15ef59b365e471b2
koflen <koflen@seznam.cz>
2022-11-07 16:55:16 +0100

    jquery modalni okno

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,27 @@
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close');
 
-button.addEventListener('click',function(){
-    modal.classList.add('modal_active');
+// button.addEventListener('click',function(){
+//     modal.classList.add('modal_active');
 
-})
+// })
 
 
-close.addEventListener('click',function(){
-    modal.classList.remove('modal_active');
-})
+// close.addEventListener('click',function(){
+//     modal.classList.remove('modal_active');
+// })
 
+$(document).ready(function(){
+    console.log('#button');
+
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    button.on('click',function(){
+        modal.addClass('modal_active');
+    });
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
+});
